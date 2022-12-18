0df653dd3c26727a4ea1e488dd0625f8b1d18111
ailzakmi <anton_2000_19@mail.ru>
2022-12-19 01:09:58 +0500

    lr4 buh

---
+++ src/lr4/Task1.java
@@ -0,0 +1,21 @@
+package lr4;
+
+public class Task1 {
+    public static void main(String[] args) {
+        int k = 11;
+        for (int i = 1 ; i <= k; i++) {
+            if (i<10) {
+                System.out.print("номер строки:  " + i + " ");
+            } else {
+                System.out.print("номер строки: " + i + " ");
+            }
+            int z = 0;
+            for (int j = -12; j < k; j++){
+                System.out.print("+");
+                z = z + 1;
+            }
+            System.out.println(" Количество символов в строке " + z);
+        }
+
+    }
+}
