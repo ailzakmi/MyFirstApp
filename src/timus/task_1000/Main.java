88c7dc53334781c18698d4651006fea170560cd3
ailzakmi <anton_2000_19@mail.ru>
2023-01-20 05:02:51 +0500

    feature/lr8

--- src/timus/task_1000/Main.java
+++ src/timus/task_1000/Main.java
@@ -1,4 +1,15 @@
 package timus.task_1000;
 
-public class Main {
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+
+        int a = in.nextInt();
+        int b = in.nextInt();
+
+        System.out.println(a + b);
+        System.out.flush();
+    }
 }
